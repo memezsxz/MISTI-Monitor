21afd60c9f4599f35a0f479b40d13cc44fd0c62b
Manal <manal.murad03@gmail.com>
2026-01-24 23:12:05 +0300

    took the notif back to its original place with navpanel

--- src/panels/NotificationsPanel.tsx
+++ src/panels/NotificationsPanel.tsx
@@ -75,7 +75,7 @@ export const NotificationsPanel = () => {
               {/* Responsive text for title */}
               <p
                 className="
-                  flex-1 min-w-0 font-semibold
+                  flex-1 min-w-0 font-semibold 
                   sm:whitespace-normal sm:break-words
                   lg:whitespace-normal lg:break-words
                 "
